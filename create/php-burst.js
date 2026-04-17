d26f755ce7afa31c9e3de37824f19ff917720736
Alex Ogunks <alexogunks@gmail.com>
2026-04-17 06:07:44 +0100

    added scripts

--- create/php-burst.js
+++ create/php-burst.js
@@ -2,9 +2,12 @@ import fetch from "node-fetch";
 import https from "https";
 import FormData from "form-data";
 
+import * as cheerio from "cheerio";
+
 const agent = new https.Agent({
   keepAlive: true,
   maxSockets: 5000,
+  rejectUnauthorized: false,
 });
 
 let running = true;
@@ -12,8 +15,37 @@ let running = true;
 const endpoint = "https://trxvault.site/register.php?ref=d11da5b0";
 
 
-const THREADS = 555;
-const BATCHES = 30;
+const THREADS = 25;
+const BATCHES = 2;
+
+const firstNames = [
+  "James","Mary","John","Patricia","Robert","Jennifer","Michael","Linda","William","Elizabeth",
+  "David","Barbara","Richard","Susan","Joseph","Jessica","Thomas","Sarah","Charles","Karen",
+  "Christopher","Nancy","Daniel","Lisa","Matthew","Betty","Anthony","Margaret","Mark","Sandra",
+  "Donald","Ashley","Steven","Kimberly","Paul","Emily","Andrew","Donna","Joshua","Michelle",
+  "Kenneth","Dorothy","Kevin","Carol","Brian","Amanda","George","Melissa","Edward","Deborah",
+  "Ronald","Stephanie","Timothy","Rebecca","Jason","Laura","Jeffrey","Sharon","Ryan","Cynthia",
+  "Jacob","Kathleen","Gary","Amy","Nicholas","Shirley","Eric","Angela","Jonathan","Helen",
+  "Stephen","Anna","Larry","Brenda","Justin","Pamela","Scott","Nicole","Brandon","Emma",
+  "Benjamin","Samantha","Samuel","Katherine","Frank","Christine","Gregory","Debra","Alexander","Rachel",
+  "Raymond","Catherine","Patrick","Carolyn","Jack","Janet","Dennis","Ruth","Jerry","Maria",
+  "Tyler","Heather","Aaron","Diane","Jose","Virginia","Adam","Julie","Nathan","Joyce",
+  "Henry","Victoria","Douglas","Olivia","Zachary","Kelly","Peter","Christina","Kyle","Lauren"
+];
+
+const lastNames = [
+  "Smith","Johnson","Williams","Brown","Jones","Garcia","Miller","Davis","Rodriguez","Martinez",
+  "Hernandez","Lopez","Gonzalez","Wilson","Anderson","Thomas","Taylor","Moore","Jackson","Martin",
+  "Lee","Perez","Thompson","White","Harris","Sanchez","Clark","Ramirez","Lewis","Robinson",
+  "Walker","Young","Allen","King","Wright","Scott","Torres","Nguyen","Hill","Flores",
+  "Green","Adams","Nelson","Baker","Hall","Rivera","Campbell","Mitchell","Carter","Roberts",
+  "Gomez","Phillips","Evans","Turner","Diaz","Parker","Cruz","Edwards","Collins","Reyes",
+  "Stewart","Morris","Morales","Murphy","Cook","Rogers","Gutierrez","Ortiz","Morgan","Cooper",
+  "Peterson","Bailey","Reed","Kelly","Howard","Ramos","Kim","Cox","Ward","Richardson",
+  "Watson","Brooks","Chavez","Wood","James","Bennett","Gray","Mendoza","Ruiz","Hughes",
+  "Price","Alvarez","Castillo","Sanders","Patel","Myers","Long","Ross","Foster","Jimenez",
+  "Powell","Jenkins","Perry","Russell","Sullivan","Bell","Coleman","Butler","Henderson","Barnes"
+];
 
 export async function send(i) {
   if (!running) return;
@@ -23,23 +55,40 @@ export async function send(i) {
   const randomNumber = (n, time) => {
     if (n == 1) return Math.floor(1 + Math.random() * 9)
     if (n == 2 && time) return Math.floor(10 + Math.random() * 60)
-      else if (!time) return Math.floor(10 + Math.random() * 99)
+      else if (n !== 2 && !time) return Math.floor(10 + Math.random() * 99)
     if (n == 4) return Math.floor(1000 + Math.random() * 9999);
     Math.floor(10000 + Math.random() * 99999)
   };
 
+  const generateFirstName = () => {
+    const c = Math.floor(Math.random() * firstNames.length);
+    const n = firstNames[c];
+    return n;
+  }
+
+  const generateLastName = () => {
+    const c = Math.floor(Math.random() * lastNames.length);
+    const n = lastNames[c];
+    return n;
+  }
+
   const generateUsername = () => {
-    const chars = 'abcdefghijklmnopqrstuvwxyz';
-    const ran = () => chars[Math.floor(Math.random() * chars.length)];
-    const email = `${ran()}${ran()}${ran()}o${ran()}${ran()}${ran()}`;
-    return email;
+    const u = `${generateFirstName()}${generateLastName()}${randomNumber(Math.floor(Math.random() * 4))}`
+    const un = u.toLowerCase();
+    return un;
   }
 
   const generateEmail = () => {
     const chars = 'abcdefghijklmnopqrstuvwxyz';
     const ran = () => chars[Math.floor(Math.random() * chars.length)];
-    const email = `${ran()}ao${ran()}${ran()}${ran()}${ran()}${Math.floor(randomNumber() / 1000)}%40gmail.com`
+    const email = `${ran()}ao${ran()}${ran()}${ran()}${ran()}${Math.floor(randomNumber() / 1000)}%40gmail.com`;
     return email;
+  }
+
+  const generatePhoneNumber = () => {
+    const random8 = () => Math.floor(10000001 + Math.random() * 89999998);
+    const num = `70${random8()}`;
+    return num;
   }
 
   function buildForm() {
@@ -67,11 +116,15 @@ export async function send(i) {
   // AOsbceaelg
 
   const countries = ['Nigeria', 'Australia', 'Canada', 'Ghana', 'India', 'Kenya', 'Tanzania', 'Uganda', 'UK', 'USA'];
-  const country = countries[Math.floor(Math.random() * countries.length)];
+  // const country = countries[Math.floor(Math.random() * countries.length)];
+  const country = 'Nigeria';
 
   const unix_timestamp = Math.floor(Date.now() / 1000);
 
   try {
+    const generatedFirstName = generateFirstName();
+    const generatedLastName = generateLastName();
+    const bothNames = `${generatedFirstName}+${generatedLastName}`
     // const res = await fetch(endpoint, {
     //   method: "POST",
     //   agent,
@@ -82,6 +135,36 @@ export async function send(i) {
     //   },
     //   body: fd
     // });
+
+    let cookie;
+
+    const ref_code = 'CE7BABE2';
+
+    const r = await fetch("https://smshikasub.com.ng/data/purchase", {
+      "headers": {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "priority": "u=1, i",
+        "sec-ch-ua": "\"Microsoft Edge\";v=\"147\", \"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"147\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-csrf-token": "lDRSFscKnsWDvwTnouljC4UaSWX9PS6xkF1JA3F5",
+        "x-requested-with": "XMLHttpRequest",
+        "cookie": "XSRF-TOKEN=eyJpdiI6InF6R3dZK2t0cVlxeFhMSnNHNTR6RlE9PSIsInZhbHVlIjoiSXdJSlBGOXBIemdNemNmOURIU1pIMnhXWGNoOEw1WlZJYlhaZzJuOUNuRk1kYXREd3B4aFNQL3NJaTA0MnpzRHFTcVVEcWZaaWpzOTZZcDZGODNmaU8wbldsVDIyYzdEOWdESUVDdWtucjdFdTRRTTVmZkdicStzOWFNbnB0ejgiLCJtYWMiOiI5MzViOTc5M2Y3OGI1ZmVlMjhjYjZhZmIzODQ0YWUxZTU1MWM4YjdhOGUyYzAxMTc0OWFlN2NhNWVhNWYyMTk0IiwidGFnIjoiIn0%3D; hk-vtu-plug-session=eyJpdiI6Ikp1U3JHUGQ5NVUrU0hxNEw1NTZKT0E9PSIsInZhbHVlIjoiT2M0cC9HeUVZWXFRMnd4TGNqNzFlUlBhcEJZdWVBZ1k4WmxNZWM2anl5UG5teEl2WElUbnFldlZwN2kxMEZTTDFuN2pLTkdVMzl4RGk2N2UzMkxFa05MMldIWFFobUVGM0xCOGpDN1lYaU44V3I0cFRHWEJ6bHVmYVF6OFFXcEsiLCJtYWMiOiI0NGRhYTQxNWVjZmY2MDcyYmM3MDIxNjhkZDJjYjZiOTYxZjQ5YTQyYzczY2YyZTFlN2ZkYzE5MDM5MDBhNTcyIiwidGFnIjoiIn0%3D",
+        "Referer": "https://smshikasub.com.ng/data"
+      },
+      "body": "_token=lDRSFscKnsWDvwTnouljC4UaSWX9PS6xkF1JA3F5&network_id=1&category_id=1&plan_id=30&phone_number=07025958800&transaction_pin=1902",
+      "method": "POST"
+    });
+
+    // const result = await r?.json() || await r?.text() || 'No result'
+    console.log(await r.text());
+
+    return;
 
     const res = await fetch("https://bullbatch.com/register", {
       "headers": {
@@ -97,12 +180,37 @@ export async function send(i) {
         "sec-fetch-site": "same-origin",
         "Referer": `https://bullbatch.com/reg/${ref_code}`
       },
-      "body": `{\"fName\":\"Alex\",\"lName\":\"${generateUsername()}\",\"email\":\"${generateEmail()}\",\"password\":\"Creeper123#\",\"country\":\"${country}\",\"currency_format\":\"en-NG\",\"currency\":\"NGN\",\"date\":\"Mar 30 at 1:${randomNumber(2, true)} PM\",\"inviter\":\"OGpouwlwha\"}`,
+      "body": `{\"fName\":\"Alex\",\"lName\":\"${generateUsername()}\",\"email\":\"${generateEmail()}\",\"password\":\"Creeper${generateUsername()}\",\"country\":\"${country}\",\"currency_format\":\"en-NG\",\"currency\":\"NGN\",\"date\":\"Mar 30 at ${randomNumber(1)}:${randomNumber(2, true)} PM\",\"inviter\":\"OGpouwlwha\"}`,
+      "method": "POST"
+    });
+    
+    const givenCookie = res.headers.get("set-cookie");
+    const signatureCookie = givenCookie.split(";")[0];
+    cookie = signatureCookie;
+    console.log(cookie);
+
+    const nextRes = await fetch("https://bullbatch.com/verify_otp", {
+      "headers": {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        "priority": "u=1, i",
+        "sec-ch-ua": "\"Chromium\";v=\"146\", \"Not-A.Brand\";v=\"24\", \"Microsoft Edge\";v=\"146\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "cookie": cookie.toString(),
+        "Referer": "https://bullbatch.com/reg/AOsbceaelg"
+      },
+      "body": "{\"otp\":\"auto\",\"screen\":\"1280wh720\"}",
       "method": "POST"
     });
 
-    console.log(await res.json());
     console.log(`Try ${i + 1} -> ${res.status}`);
+    console.log(await res.json());
+    console.log(await nextRes.json());
     return;
 
     const transferRes = await fetch("https://bullbatch.com/bullbatch_to_bullbatch", {
